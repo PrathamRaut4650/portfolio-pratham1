5d2680ab1f016cc901b532d0d4d825430bcea779
Pratham Raut <rautpratham84@gmail.com>
2024-11-30 11:09:31 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
     <div id ='home'className='hero'>
-        <h1><span>I'm Pratham Raut, </span>FrontEnd Developer From Mumbai</h1>
-        <p>I am a frontend Developer from Mumbai,Graduated from Patkar Varde College in degree of BScIT.</p>
+        <h1><span>I'm Pratham Raut, </span>Full Stack Developer From Mumbai</h1>
+        <p>I am a Full Stack Developer from Mumbai,Graduated from Patkar Varde College in degree of BScIT.</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link'offset={50} href='#contact'>Connect With Me</AnchorLink></div>
             <div className="hero-resume">My resume</div>
